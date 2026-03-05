4d222080b4e67bbdf95018e6ac13ffd71399312e
Icaro Oliveira <icaro.vinicius.981@gmail.com>
2026-03-05 01:13:58 -0300

    feat(task): implement show, update and destroy endpoints

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
 use App\Services\TaskService;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -17,15 +20,34 @@ class TaskController extends Controller
         $this->taskService = $taskService;
     }
 
-    public function index()
+    public function index(): Collection
     {
         return Task::all();   
     }
     
-    public function store(StoreTaskRequest $request, int $projectId)
+    public function store(StoreTaskRequest $request, int $projectId): Task
     {
         $data = $request->validated();
 
         return  Task::create(array_merge($data, ['project_id' => $projectId])); 
+    }
+
+    public function show(Task $task): Task
+    {
+        return $task;
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $data = $request->validated();
+        
+        return $task->update($data);
+    }
+
+    public function destroy(Task $task): JsonResponse
+    {
+        $task->delete();
+
+        return response()->json(['message' => 'Task deleted successfully']);
     }
 }
