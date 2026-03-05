3c0b785e788b07aaa3ed5b02c156c05a7774f9e3
Icaro Oliveira <icaro.vinicius.981@gmail.com>
2026-03-05 01:07:51 -0300

    feat(task): implement index and store endpoint

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Models\Task;
+use App\Services\TaskService;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    protected $taskService;
+
+    public function __construct(TaskService $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
+    public function index()
+    {
+        return Task::all();   
+    }
+    
+    public function store(StoreTaskRequest $request, int $projectId)
+    {
+        $data = $request->validated();
+
+        return  Task::create(array_merge($data, ['project_id' => $projectId])); 
+    }
 }
