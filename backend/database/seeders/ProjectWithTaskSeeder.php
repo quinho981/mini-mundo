8fba6ed317ae1f2451eb9351170a4e5cf3703ce2
Icaro Oliveira <icaro.vinicius.981@gmail.com>
2026-03-06 21:50:36 -0300

    feat(seeders): create project and task seeders

---
+++ backend/database/seeders/ProjectWithTaskSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectWithTaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Project::factory()
+            ->count(2)
+            ->create()
+            ->each(fn ($project) =>
+                $project->tasks()->saveMany(
+                    \App\Models\Task::factory()->count(rand(1, 3))->make()
+                )
+            );
+    }
+}
